106805f0f18f3f72354c9f27cdb356bcb25159f5
Dan Sweeney <dan@floatingboxes.com>
2013-08-12 14:50:22 -0500

    updates to use css fixed positioning instead of jquery re-positioning on scroll (much smoother on mobile)

--- js/masthead.js
+++ js/masthead.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
 
 	createDisclosureButtons();
 
-
 	$('.menu__link').click(function(e) {
 		e.preventDefault();
 		toggleMenu();
+		toggleMastheadPostion();
 	});
 	
 
@@ -18,13 +18,9 @@ $(document).ready(function() {
 
 
 	$(document).scroll(function() {
-		if (!menuIsOpen()) { 
-			lockMastheadToTop();
-		}
-
 		if (menuIsOpen() && userScrollsAboveMenu()) {
 			toggleMenu();
-			lockMastheadToTop();
+			toggleMastheadPostion();
 		}
 		
 	});
@@ -37,18 +33,32 @@ $(document).ready(function() {
 // Functions =================================================
 
 
+	function changeMastheadToFixedPosition(){
+		$('.masthead').css("position", "fixed");
+	}
+
 	function toggleMenu(){
 		$('#menu').toggleClass('active');
 		$('.menu__link').toggleClass('active');
 		$('.masthead').toggleClass('menu-is-open');
-	};
+	}
 
+	function toggleMastheadPostion() {
+		$('.masthead').toggleClass('absolute');
+
+		if ($('.masthead').hasClass('absolute')){
+			var scrollTop = $(window).scrollTop();
+			$('.masthead').css("top", scrollTop);
+		} else {
+			$('.masthead').css("top", 0);
+		}
+	}
 
 	function createDisclosureButtons() {
 		var $dropdownLink = $('.has-subnav > a');
 		var disclosureLink = "<a class=\"disclosure-button\" href=\"#\">+</a>";
 		$dropdownLink.after(disclosureLink);	
-	};
+	}
 
 
 	function toggleDisclosureButtonIcon(icon) {
@@ -62,11 +72,6 @@ $(document).ready(function() {
 	function menuIsOpen() {
 		return $('.menu-is-open').length > 0;
 	}
-
-	function lockMastheadToTop() {
-		var scrollTop = $(window).scrollTop();
-		$('.masthead').css("top", scrollTop);
-	};
 
 
 	function userScrollsAboveMenu() {
